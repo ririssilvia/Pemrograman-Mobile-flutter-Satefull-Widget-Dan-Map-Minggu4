9d5aef1f8feb09beff31b0cb96eee4a286f09260
ririssilvia <ririssilvia1@gmail.com>
2021-03-04 22:32:41 +0700

    first commit-Satefull Widget Dan Map

--- lib/RiwayatKonversi.dart
+++ lib/RiwayatKonversi.dart
@@ -11,15 +11,15 @@ class RiwayatKonversi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-                  children: listViewItem.map((String value) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      child: Text(
-        value,
-        style: TextStyle(fontSize: 15),
-      ),
+      children: listViewItem.map((String value) {
+        return Container(
+          margin: EdgeInsets.all(10),
+          child: Text(
+            value,
+            style: TextStyle(fontSize: 15),
+          ),
+        );
+      }).toList(),
     );
-                  }).toList(),
-                );
   }
 }
